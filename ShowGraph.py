ef6978893fe2ab73c04ea549b1bdf541848dae1c
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-06-25 16:49:51 +0100

    Fixed text boxes overlapping

--- ShowGraph.py
+++ ShowGraph.py
@@ -6,19 +6,15 @@ from matplotlib.widgets import RadioButtons, Cursor
 
 def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMPH, AvgPacesKM, AvgPacesMiles):
 
-    Selections = [Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMPH, AvgPacesKM, AvgPacesMiles]
-    # so that the values plotted on the y-axis can be changed
-
     fig, ax = plt.subplots(figsize = (10,5), dpi = 100, num="Running Tracker")
     # defines the subplots, and the window size and title
     plt.subplots_adjust(left=0.4, right = 0.88)
     # puts empty space to the left of the plot so that the buttons can go there
 
-
     converted_dates = list(map(datetime.datetime.strptime, Dates, len(Dates)*['%d/%m/%Y']))
     # converts the date strings to date objects
 
-    plot, = ax.plot(converted_dates, Selections[0], marker = 'o', mfc = 'w', linestyle = 'dashed')
+    plot, = ax.plot(converted_dates, Durations, marker = 'o', mfc = 'w', linestyle = 'dashed')
     # plots the points
 
     fig.autofmt_xdate()
@@ -33,11 +29,16 @@ def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMP
     fig.text(0.65, 0.9, "Hover over a point to see exact details!", fontsize = 8, color="black")
     # sets titles for axis and plot
 
+    minyvalue = float(min(plot.get_ydata())) * 0.9
+    maxyvalue = float(max(plot.get_ydata())) * 1.1
+    ax.set_ylim(minyvalue, maxyvalue)
+    # this finds the limits for the y-axis, it will be limited to 10% either side of the data
+
     fig.set_facecolor("#ffffcc")
     # changes background colour of plot
 
     average = round(sum(plot.get_ydata())/len(plot.get_ydata()),2)
-    fig.text(0.025,0.3,"The average duration is " + str(average) + " minutes.", fontsize=12, color = 'w', bbox={"facecolor":"b", "alpha":0.5})  
+    fig.text(0.025, 0.2,"The average duration is " + str(average) + " minutes.", fontsize=12, color = 'w', bbox={"facecolor":"b", "alpha":0.5})  
     # finds the average for the y-attribute chosen and displays it to screen
 
 
@@ -45,18 +46,6 @@ def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMP
     databuttons = RadioButtons(databuttonsloc, ('Durations (minutes)', 'Distances (km)', 'Distances (miles)', 'Average Speeds (km/h)', 'Average Speeds (mph)', 'Average Paces (min/km)', 'Average Paces (min/mile)'), active = 0)
     # creates and shows buttons so the user can choose what y-attribute is shown
 
-
-    for i in range(len(Dates)):
-        annot = ax.annotate("", xy = (0,0), bbox = dict(boxstyle = 'round4',fc='linen',ec='k',lw=1))
-        individualdate = (plot.get_xdata()[i])
-        datexcoord = mpl.dates.date2num(individualdate)
-
-        annot.xy = (datexcoord, Durations[i])
-
-        text = "({:},{:}{:})".format(Dates[i], " ", Durations[i])
-
-        annot.set_text(text)
-        annot.set_visible(True) 
 
     def onclickpoint(event):
         x = event.xdata
@@ -66,7 +55,8 @@ def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMP
             individualdate = (plot.get_xdata()[i])
             datexcoord = mpl.dates.date2num(individualdate)
             annot = ax.annotate("({:},{:}{:})".format(Dates[i], " ", plot.get_ydata()[i]), xy = (datexcoord, plot.get_ydata()[i]), bbox = dict(boxstyle = 'square', fc='#ffffcc', ec='#0052cc', lw=1))
-
+            annot.set_visible(False)
+            # turning them all to invisible first, then making the one (if any) being hovered over visible
             try: 
                 if x - 2 < datexcoord < x + 2:
                     if y - 0.5 < plot.get_ydata()[i] < y + 0.5:
@@ -74,21 +64,19 @@ def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMP
                         break
 
                 for child in ax.get_children():
-                        if isinstance(child, mpl.text.Annotation):
-                            child.remove()
+                    if isinstance(child, mpl.text.Annotation):
+                        child.remove()
+                        # removes any annotations from screen that have previously been hovered over, but are no longer
             except:
-                for child in ax.get_children():
-                        if isinstance(child, mpl.text.Annotation):
-                            child.remove()
+                pass
                 
-
-
         fig.canvas.draw()
         plt.show()
 
     fig.canvas.mpl_connect('motion_notify_event', onclickpoint)
 
     def drawGraph(label):
+        # the button that is clicked is passed in as the label parameter
 
         labeldict = {'Durations (minutes)': Durations,
         'Distances (km)': DistancesKM,
@@ -99,7 +87,7 @@ def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMP
         'Average Paces (min/mile)': AvgPacesMiles}
         # dictionary to link the labels to the equivalent data
 
-
+        print(label)
         ax.set_ylabel(label, fontsize = 14)
         plot.set_ydata(labeldict[label])
         # the y-label and data is changed to whichever button was pressed
@@ -133,8 +121,9 @@ def run(Dates, Durations, DistancesKM, DistancesMiles, AvgSpeedsKMH, AvgSpeedsMP
         ax.set_ylim(minyvalue, maxyvalue)
         # this finds the limits for the y-axis, it will be limited to 10% either side of the data
 
-        del fig.texts[0]  
+        del fig.texts[1]  
         # the average text is deleted to they do not stack on top of each other
+        # the second text is deleted (index 1) because index 0 refers to the text saying "hover over a point..."
 
         plt.draw()
         # the graph is updated with the changes
